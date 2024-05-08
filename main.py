817ce2d0c048fb814e266f5dac18abacd1f70f45
Juan <jdrobayot@udistrital.edu.co>
2024-05-08 14:28:13 -0500

    Added Factor de Certeza

--- main.py
+++ main.py
@@ -4,9 +4,13 @@ allHypothesis = {}
 
 def validateRule(condition):
     facts = [
-        { "A": "true" },
-        { "B": "true" },
-        { "C": "true" },
+        {"Leche": "false"},
+        {"Pelo": "true"},
+        {"Carne": "Come"},
+        {"Dientes": "Puntiagudos"},
+        {"Garras": "true"},
+        {"Ojos": "Frente"},
+        {"Color": "leonado_rayas"},
     ]
 
     for fact in facts:
@@ -15,28 +19,63 @@ def validateRule(condition):
                 if condition.attribute == attribute and condition.value == value: return True
             else:
                 if condition.get("attribute") == attribute and condition.get("value") == value: return True
-                
+
     return False
 
+conditions_results = []
+
+def evaluateResults():
+    global conditions_results
+    if all(conditions_results):
+        print("El objetivo es definitivamente Verdadero")
+    else:
+        print("El objetivo es definitivamente Falso")
+
 def backwardChaining(hypothesis):
-    band = False
+    global conditions_results
+
     for condition in hypothesis.conditions:
         if isinstance(condition, Node):
             print("Hipotesis: " + condition.attribute + " => " + condition.value)
             backwardChaining(condition)
         else:
             print("Condicion: " + condition.get("attribute") + " => " + condition.get("value"))
-            band = validateRule(condition)
-            print("Condicion en base de hechos: " + str(band) + "\n")
+            result = validateRule(condition)
+            conditions_results.append(result)
+            print("Condicion en base de hechos: " + str(result) + "\n")
 
 def loadExample():
-    allHypothesis[1] = Node("D", "true", [{ "attribute": "A", "value": "true"}])
-    allHypothesis[2] = Node("F", "true", [allHypothesis[1], { "attribute": "B", "value": "true"}])
-    allHypothesis[3] = Node("H", "true", [allHypothesis[2], { "attribute": "G", "value": "true"}])
-    allHypothesis[4] = Node("I", "true", [{ "attribute": "C", "value": "true"}])
-    allHypothesis[5] = Node("W", "true", [allHypothesis[3], allHypothesis[4]])
-    allHypothesis[6] = Node("W", "true", [allHypothesis[3], { "attribute": "J", "value": "true"}])
-    allHypothesis[7] = Node("G", "true", [{ "attribute": "B", "value": "true"}])
+    allHypothesis[1] = Node("Mamifero", "true", [{"attribute": "Pelo", "value": "true"}])
+    allHypothesis[2] = Node("Mamifero", "true", [allHypothesis[1], {"attribute": "Leche", "value": "true"}])
+    allHypothesis[3] = Node("Ave", "true", [{"attribute": "Plumas", "value": "true"}])
+    allHypothesis[4] = Node("Ave", "true", [allHypothesis[3], {"attribute": "Huevos", "value": "true"}])
+    allHypothesis[5] = Node("Carnivoro", "true", [{"attribute": "Carne", "value": "Come"}])
+    allHypothesis[6] = Node("Carnivoro", "true", [allHypothesis[5], {"attribute": "Dientes", "value": "Puntiagudos"},
+                                                  {"attribute": "Garras", "value": "true"},
+                                                  {"attribute": "Ojos", "value": "Frente"}])
+    allHypothesis[7] = Node("Ungulado", "true", [allHypothesis[2], {"attribute": "Pezuñas", "value": "true"}])
+    allHypothesis[8] = Node("Ungulado", "true", [allHypothesis[2], {"attribute": "Rumia", "value": "true"}])
+    allHypothesis[9] = Node("Leopardo", "true",
+                            [allHypothesis[2], allHypothesis[6], {"attribute": "Color", "value": "leonado_manchas"}])
+    allHypothesis[10] = Node("Tigre", "true",
+                             [allHypothesis[2], allHypothesis[6], {"attribute": "Color", "value": "leonado_rayas"}])
+    allHypothesis[11] = Node("Jirafa", "true", [allHypothesis[8], {"attribute": "Cuello", "value": "largo"},
+                                                {"attribute": "Piernas", "value": "largo"},
+                                                {"attribute": "machas", "value": "true"}])
+    allHypothesis[12] = Node("Cebra", "true", [allHypothesis[8], {"attribute": "Rayas", "value": "Negras"}])
+    allHypothesis[13] = Node("Avestruz", "true", [allHypothesis[4], {"attribute": "Vuela", "value": "false"},
+                                                  {"attribute": "Cuello", "value": "largo"},
+                                                  {"attribute": "Piernas", "value": "largo"},
+                                                  {"attribute": "Color", "value": "blanco_negro"}])
+    allHypothesis[14] = Node("Pinguino", "true", [allHypothesis[4], {"attribute": "Vuela", "value": "false"},
+                                                  {"attribute": "Nada", "value": "true"},
+                                                  {"attribute": "Cuello", "value": "corto"},
+                                                  {"attribute": "Piernas", "value": "corto"},
+                                                  {"attribute": "Color", "value": "blanco_negro"}])
+    allHypothesis[15] = Node("Albatros", "true", [allHypothesis[4], {"attribute": "Vuela", "value": "true"}])
+    allHypothesis[16] = Node("Misma_especie", "true", [{"attribute": "Padre", "value": "true"}])
+
+
 
 def menuBackwardChaining():
     print("\n\n--- ENCADENAMIENTO HACIA ATRAS ---")
@@ -46,75 +85,31 @@ def menuBackwardChaining():
     num = int(input("Escoja la hipotesis: "))
 
     backwardChaining(allHypothesis[num])
-    
 
-def menuAddCondition(): 
+
+def menuAddCondition():
     attribute = str(input("Ingresa un atributo: "))
     value = str(input("Ingresa un valor: "))
 
-    #print("Regla creada:", "ATRIBUTO: ", attribute, "VALOR: ", value)
-    
-    return { "attribute": attribute, "value": value }
+    # print("Regla creada:", "ATRIBUTO: ", attribute, "VALOR: ", value)
 
-
-def menuAddHypothesis():
-    print("\n\n--- CREAR HIPOTESIS ---")
-    attribute = str(input("Ingresa un atributo: "))
-    value = str(input("Ingresa un valor: "))
-
-
-    hypothesis = Node(attribute, value)
-    print("Hipotesis creada: ", "ATRIBUTO: ", hypothesis.attribute, "VALUE: ", hypothesis.value)
-
-    allHypothesis[len(allHypothesis) + 1] = hypothesis
-    print(allHypothesis)
-
-    while True:
-        print("\n --- MENU DE OPCIONES DE HIPOTESIS ", hypothesis.attribute, " ---")
-        print("1. Agregar condicion")
-        #print("2. TODO:Agregar hipotesis como condicion")
-        print("3. Salir")
-
-        option_hyp = input("Ingrese una opcion: ")
-
-        if int(option_hyp) == 1:
-            hypothesis.addCondition(menuAddCondition())
-        if int(option_hyp) == 3:
-            print("\nSaliendo... ")
-            break
+    return {"attribute": attribute, "value": value}
 
 
 if __name__ == '__main__':
-    # Si el nodo tiene condiciones es una hipotesis, de lo contrario es solo una condición
-    # conditionsPerro = [ { "attribute": "num_patas", "value": "4" }, { "attribute": "pelaje", "value": "corto" } ]
-    
-    # hypothesisPerro = Node(
-    #     "animal", 
-    #     "perro", 
-    #     conditionsPerro
-    # )
-
-    # conditionsPitbull = [
-    #     { "attribute": "tamaño", "value": "pequeño" }, 
-    #     hypothesisPerro
-    # ]
-
-    # hypothesisPitbull = Node("raza", "pitbull", conditionsPitbull)
 
     while True:
         print("\n\nMENU DE OPCIONES")
-        print("1. Crear Hipotesis")
-        print("2. Encadenamiento hacia atras")
-        print("3. Cargar ejemplo")
+        print("1. Encadenamiento hacia atras")
+        print("2. Cargar Base de conocimientos")
         print("9. Salir")
 
         option = input("Ingresa una opcion: ")
 
         if int(option) == 1:
-            menuAddHypothesis()
-        if int(option) == 2:
             menuBackwardChaining()
-        if int(option) == 3:
+            evaluateResults()
+        if int(option) == 2:
             loadExample()
         if int(option) == 9:
             print("\nSaliendo... ")
@@ -122,4 +117,3 @@ if __name__ == '__main__':
 
     # Se pasa la hipotesis a validar
     # backwardChaining(hypothesisPitbull)
-
